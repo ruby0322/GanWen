ac803532f354fb0c9c96452f2efd9e0ef8a3988e
Ruby <ruby0322@ntu.im>
2022-02-02 15:19:35 +0800

    .

--- app.py
+++ app.py
@@ -7,23 +7,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
-    return 'hello, world!'
+    return """Hello, welcome to NTU department API by ruby0322.
+Please visit https://ntu-dept-code.herokuapp.com/dept_code_api for NTU Department Code API.
+Please visit https://ntu-dept-code.herokuapp.com/dept_name_api for NTU Department Name API.
+Please visit https://hackmd.io/@ruby0322/BJjgtcD0F for the official API Docs."""
 
-@app.route('/api', methods=['GET'])
-def api() -> str:
+@app.route('/dept_code_api', methods=['GET'])
+def dept_code_api() -> dict:
 
     args = request.args
 
-    if 'dept_name' in args:
-        dept_name = args['dept_name']
-        print(dept_name)
-    else:
-        dept_name = ''
-        print('Dept name missing.')
+    return Result(
+        **{
+            'query_type': Result.QUERY_TYPE_DEPT_CODE,
+            'dept_name': args['dept_name'] if 'dept_name' in args else ''
+        }
+    ).as_json_dict()
 
-    return Result(dept_name).as_json_dict().__repr__()
+@app.route('/dept_name_api', methods=['GET'])
+def dept_name_api() -> dict:
+
+    args = request.args
+
+    return Result(
+        **{
+            'query_type': Result.QUERY_TYPE_DEPT_NAME,
+            'dept_code': args['dept_code'] if 'dept_code' in args else '',
+            'name_format': args['name_format'] if 'name_format' in args else 'short'
+        }
+    ).as_json_dict()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
     
--- result.py
+++ result.py
@@ -1,27 +1,42 @@
 import json
+from unicodedata import name
 
 
 
 
 class Result:
 
-    OK_MESSAGE = '[OK] Department code found successfully.'
-    NOT_FOUND_MESSAGE = '[NOT FOUND] Cannot find the corresponding department code.'
+    OK_MESSAGE = '[OK] Department code/name found successfully.'
+    NOT_FOUND_MESSAGE = '[NOT FOUND] Cannot find the corresponding department code/name.'
     EMPTY_QUERY_MESSAGE = '[NOT FOUND] An empty query string is provided.'
     ERROR_MESSAGE = '[ERROR] Some unknown error occurred during the process'
 
     STATUS_CODE = {}
     DEPT_CODE = {}
 
+    QUERY_TYPE_DEPT_CODE = 0
+    QUERY_TYPE_DEPT_NAME = 1
+
     inited = False
 
-    def __init__(self, dept_name: str) -> None:
+    def __init__(self, dept_name: str='', dept_code: str='', query_type: str='', name_format: str='') -> None:
 
         if not Result.inited:
             Result.init()
 
         try:
-            self.result = self.find_dept_code(dept_name=dept_name)
+            if query_type == Result.QUERY_TYPE_DEPT_CODE:
+                self.result = self.find_dept_code(dept_name=dept_name)
+                self.query_type = 'dept_code'
+                self.query_string = dept_name
+            elif query_type == Result.QUERY_TYPE_DEPT_NAME:
+                self.result = self.find_dept_name(dept_code=dept_code, name_format=name_format)
+                self.query_type = 'dept_name'
+                self.query_string = dept_code
+            else:
+                self.query_type = ''
+                self.query_string = ''
+                raise Exception()
 
             if self.result:
                 self.status_code = Result.STATUS_CODE['OK']
@@ -34,6 +49,17 @@ class Result:
             self.status_code = Result.STATUS_CODE['ERROR']
             self.message = Result.ERROR_MESSAGE
 
+    def find_dept_name(self, dept_code: str, name_format: str) -> str:
+        if dept_code in Result.DEPT_CODE:
+            index = 0
+            if name_format == 'full':
+                index = 0
+            elif name_format == 'short':
+                index = 1
+            return Result.DEPT_CODE[dept_code][index]
+
+        return ''
+
     def find_dept_code(self, dept_name: str) -> str:
 
         for key, names in Result.DEPT_CODE.items():
@@ -45,6 +71,8 @@ class Result:
     def as_json_dict(self) -> dict:
         return {
             'status_code': self.status_code,
+            'query_type': self.query_type,
+            'query_string': self.query_string,
             'result': self.result,
             'message': self.message,
         }
