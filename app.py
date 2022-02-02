372638e7933576c258ee1fe81d9436544e9e650d
Ruby <ruby0322@ntu.im>
2022-02-02 15:25:33 +0800

    .

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def dept_name_api() -> dict:
     ).as_json_dict()
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
     
