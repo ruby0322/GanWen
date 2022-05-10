5eb4840edf4bdf767624a3a1f8428938a34fce7a
Ruby <ruby0322@ntu.im>
2022-05-10 15:21:18 +0800

    .

--- config.py
+++ config.py
@@ -10,10 +10,7 @@ HALF_COMMA = ','
 HALF_PERIOD = '.'
 HALF_EXCLAMATION = '!'
 
-PUNCTUATION_POOL = [
-    FULL_COMMA, FULL_PERIOD, FULL_EXCLAMATION, 
-    HALF_COMMA, HALF_PERIOD, HALF_EXCLAMATION
-]
+PUNCTUATION_POOL = '，。！,.!\n '
 
 def generate_random_emoji_sequence() -> str:
     return ' ' + ''.join([random.choice(EMO_POOL)*2 for _ in range(2)]) + ' '
