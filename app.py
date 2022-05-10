8daf590109ac7e7b27a395550502339f8730585e
Ruby <ruby0322@ntu.im>
2022-05-10 11:55:00 +0800

    make it better

--- app.py
+++ app.py
@@ -1,44 +1,18 @@
 import os
 from flask import Flask, request, abort
-from result import Result
-
+from config import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
-    return """Hello, welcome to NTU department API by ruby0322.
-Please visit https://ntu-dept-code.herokuapp.com/dept_code_api for NTU Department Code API.
-Please visit https://ntu-dept-code.herokuapp.com/dept_name_api for NTU Department Name API.
-Please visit https://hackmd.io/@ruby0322/BJjgtcD0F for the official API Docs."""
+    return """Hello, welcome to GAN API."""
 
-@app.route('/dept_code_api', methods=['GET'])
+@app.route('/gan_text', methods=['GET'])
 def dept_code_api() -> dict:
-
-    args = request.args
-
-    return Result(
-        **{
-            'query_type': Result.QUERY_TYPE_DEPT_CODE,
-            'dept_name': args['dept_name'] if 'dept_name' in args else ''
-        }
-    ).as_json_dict()
-
-@app.route('/dept_name_api', methods=['GET'])
-def dept_name_api() -> dict:
-
-    args = request.args
-
-    return Result(
-        **{
-            'query_type': Result.QUERY_TYPE_DEPT_NAME,
-            'dept_code': args['dept_code'] if 'dept_code' in args else '',
-            'name_format': args['name_format'] if 'name_format' in args else 'short'
-        }
-    ).as_json_dict()
+    return convert(request.args.get('to_convert'))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     # app.run()
-    
---
+++ config.py
@@ -0,0 +1,30 @@
+import random
+
+EMO_POOL = ['😂', '👍', '🤙', '😎', '😳', '💪', '👌', '🖖', '🈹',' ⚡', '💦']
+GAN_POOL = [' skrskr🤙🤙 ', ' 很厲害欸😎😎 ', ' peace ', ' 笑死😂😂 ', ' 哈哈是我啦 ']
+
+FULL_COMMA = '，'
+FULL_PERIOD = '。'
+FULL_EXCLAMATION = '！'
+HALF_COMMA = ','
+HALF_PERIOD = '.'
+HALF_EXCLAMATION = '!'
+
+PUNCTUATION_POOL = [
+    FULL_COMMA, FULL_PERIOD, FULL_EXCLAMATION, 
+    HALF_COMMA, HALF_PERIOD, HALF_EXCLAMATION
+]
+
+def generate_random_emoji_sequence() -> str:
+    return ''.join([random.choice(EMO_POOL)*2 for _ in range(2)])
+
+def generate_random_gan_text() -> str:
+    return random.choice(GAN_POOL)
+
+def convert(string: str) -> str:
+    for punctuation in PUNCTUATION_POOL:
+        punctuation_cnt = string.count(punctuation)
+        for _ in range(punctuation_cnt):
+            additive_gen = random.choice([generate_random_emoji_sequence, generate_random_gan_text])
+            string = string.replace(punctuation, additive_gen(), 1)
+    return string
