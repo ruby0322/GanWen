df99c5ac743181ca5ef19703bcf7b788d1b67adf
Ruby <ruby0322@ntu.im>
2022-05-10 13:42:05 +0800

    .

---
+++ static/js/gan_wen_api.js
@@ -0,0 +1,21 @@
+const copy = () => {
+    document.querySelector('#output').select();
+    document.execCommand('Copy');
+};
+const displayConverted = data => {
+    const converted = data['converted'];
+    document.querySelector('#output').value = converted;
+    document.querySelector('#output').innerHTML = converted;
+    console.log(converted);
+};
+const fetchGanTextAPI = async toConvert => {
+    apiUrl = `https://ganwenapi.herokuapp.com/gan_text?to_convert=${toConvert}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayConverted(data));
+};
+const convert = async () => {
+    const toConvert = document.querySelector('#input').value;
+    fetchGanTextAPI(toConvert);
+};
